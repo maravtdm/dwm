5c485fc2abc2ba2424caa054593d9db03ded75dd
marav <maravtdm@users.noreply.github.com>
2018-12-26 01:06:19 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -11,7 +11,6 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-/*static const char col_cyan[]        = "#005577";*/
 static const char col_cyan[]        = "#546E7A";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
