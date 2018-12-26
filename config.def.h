8be38c9460fc028c40ffbd711a452387894dd045
marav <maravtdm@users.noreply.github.com>
2018-12-26 01:07:57 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const char col_cyan[]        = "#546e7a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
 };
 
 /* tagging */
