a484cd2fd46a687494333c0ff0194d113e0a65aa
marav <maravtdm@users.noreply.github.com>
2019-01-11 18:54:50 +0100

    Update config.h

--- config.h
+++ config.h
@@ -84,8 +84,8 @@ static const char *cajacmd[] = { "caja", NULL };
 static const char *volmcmd[] = { "pamixer", "-t", NULL };
 static const char *voldcmd[] = { "pamixer", "-d", "10", NULL };
 static const char *volucmd[] = { "pamixer", "-i", "10", NULL };
-static const char *lightucmd[] = { "xbacklight", "-inc", "+10%", NULL };
-static const char *lightdcmd[] = { "xbacklight", "-dec", "-10%", NULL };
+static const char *lightucmd[] = { "xbacklight", "-inc", "10", NULL };
+static const char *lightdcmd[] = { "xbacklight", "-dec", "10", NULL };
 static const char *lockcmd[] = { "/home/david/.config/i3/lockscreen", "off", NULL };
 static const char *printcmd[] = { "escrotum", "-s", "/home/david/Images/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL };
 
