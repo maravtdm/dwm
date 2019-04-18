16e2328ca28f362d7d23626dd94736700d5339d0
marav <maravtdm@users.noreply.github.com>
2019-04-18 02:28:30 +0200

    Update config.h

--- config.h
+++ config.h
@@ -35,7 +35,6 @@ static const Rule rules[] = {
 	{ "Tilda",    NULL,       NULL,       0,            1,           -1 },
 	{ "Guake",    NULL,       NULL,       0,            1,           -1 },
 	{ "Galculator",    NULL,  NULL,       0,            1,           -1 },
-	{ "Xfce4-terminal", NULL,  NULL,	      0,	    1,		 -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Firefox Developer Edition",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Firefox",  NULL,	  "À propos de Mozilla Firefox",        1 << 8,         1,     -1 },
