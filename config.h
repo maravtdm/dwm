5e0c9429e0f55565501ce6cbbb5565f7383dfcca
maravtdm <dav.jagu@gmail.com>
2019-09-27 03:20:43 +0200

    update_20190919

--- config.h
+++ config.h
@@ -93,7 +93,8 @@ static const char *volucmd[] = { "pamixer", "-i", "10", NULL };
 static const char *lightucmd[] = { "xbacklight", "-inc", "10", NULL };
 static const char *lightdcmd[] = { "xbacklight", "-dec", "10", NULL };
 static const char *lockcmd[] = { "/home/david/.config/i3/lockscreen", "off", NULL };
-static const char *printcmd[] = { "import", "-screen", "/home/marav/Images/Screenshots/`date -I`_screenshot.png", NULL };
+/*static const char *printcmd[] = { "import", "-screen", "/home/marav/Images/Screenshots/`date -I`_screenshot.png", NULL }; */
+static const char *printcmd[] = { "scrot", "-s", "-e", "mv", "$f", "/home/marav/Images/Screenshots/", NULL };
 static const char *xfcecmd[] = { "xfce4-terminal", "--drop-down", NULL };
 
 static Key keys[] = {
