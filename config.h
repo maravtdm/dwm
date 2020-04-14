a65551bc6329e613228e19db8b6f24e78ac8f053
maravtdm <dav.jagu@gmail.com>
2020-04-14 20:02:04 +0200

    update_20200414-20:02:04

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char col_cyan[]        = "#005577";
 static const char col_red[]         = "#f90000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_white },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan, col_cyan },
 };
 
