8c0d63782490ee4ab186b8bd748a2a8f73444ce4
maravtdm <dav.jagu@gmail.com>
2020-04-14 19:58:48 +0200

    update_20200414-19:58:48

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 1;   /* systray spacing */
@@ -20,7 +20,7 @@ static const char col_red[]         = "#F90000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan, col_red },
+	[SchemeSel]  = { col_gray4, col_cyan, col_cyan },
 };
 
 
